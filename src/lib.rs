a3e4617d9bc46e8a1ce2b440bafa3215ee568937
LJason <ljason@ljason.cn>
2021-10-20 00:24:39 +0800

    优化代码
    Signed-off-by: LJason <ljason@ljason.cn>

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,33 @@
 #![deny(clippy::pedantic)]
-#![allow(clippy::non_ascii_literal)]
+#![allow(clippy::non_ascii_literal, clippy::must_use_candidate)]
+
+use std::cmp::Ordering;
 
 /// 更相减损术
 pub fn subtraction(mut x: u64, mut y: u64) -> u64 {
-    while x != y {
-        if x > y {
-            x -= y
-        } else {
-            y -= x
+    loop {
+        match x.cmp(&y) {
+            Ordering::Less => y -= x,
+            Ordering::Greater => x -= y,
+            Ordering::Equal => return x,
         }
     }
-    x
 }
 
 /// 更相减损术 除2
 pub fn subtraction2(mut x: u64, mut y: u64) -> u64 {
     let mut i = 1;
     while x % 2 == 0 && y % 2 == 0 {
-        x = x / 2;
-        y = y / 2;
-        i = i * 2;
+        x /= 2;
+        y /= 2;
+        i *= 2;
     }
 
-    while x != y {
-        if x > y {
-            x -= y;
-        } else if y > x {
-            y -= x;
+    loop {
+        match x.cmp(&y) {
+            Ordering::Less => y -= x,
+            Ordering::Greater => x -= y,
+            Ordering::Equal => return x * i,
         }
     }
-    x * i
 }
